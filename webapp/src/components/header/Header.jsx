a86bac49e53b07997cf8607fa1e23c24134753c3
LuizMaestri <luizricardomaestri@gmail.com>
2018-07-31 01:42:25 -0300

    add link to / on navbar

--- webapp/src/components/header/Header.jsx
+++ webapp/src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 import logo from './logo.png';
 
 export default (props) => (
     <Navbar color="light" light>
-        <NavbarBrand href="#">
-            <img src={logo} alt="Logo" height="35px"/>
+        <NavbarBrand>
+            <NavLink to="/">
+                <img src={logo} alt="Logo" height="35px"/>
+            </NavLink>
         </NavbarBrand>
     </Navbar>
 );
