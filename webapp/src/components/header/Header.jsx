d2cfec76a4b18241c485f42042b06e4972a57fa1
LuizMaestri <luizricardomaestri@gmail.com>
2018-08-02 00:25:17 -0300

    fix html error <a> inside <a>

--- webapp/src/components/header/Header.jsx
+++ webapp/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import logo from './logo.png';
 
 export default (props) => (
     <Navbar color="light" light>
-        <NavbarBrand>
+        <NavbarBrand tag="button" className="btn btn-outline-secondary" style={{ borderWidth: '0'} }>
             <NavLink to="/">
                 <img src={logo} alt="Logo" height="35px"/>
             </NavLink>
