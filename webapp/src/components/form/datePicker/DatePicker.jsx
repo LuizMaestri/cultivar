8cf915fcc7c60f7db8466147cc5695da39863773
LuizMaestri <luizricardomaestri@gmail.com>
2018-08-05 17:09:32 -0300

    new datepicker component

---
+++ webapp/src/components/form/datePicker/DatePicker.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormGroup, Input, Label, FormFeedback } from 'reactstrap';
+import './datePicker.css';
+
+const Picker = props => (
+    <FormGroup>
+        <Label for={props.id}>{props.label}</Label>
+        <Input id={props.id} type="date" max={new Date().toISOString().substr(0, 10)} 
+            placeholder={props.placeholder ? props.placeholder : "DD/MM/AAAA"} onChange={event => props.onchange } {...props.err} />
+        <FormFeedback>{props.errMessage}</FormFeedback>
+    </FormGroup>
+);
+
+Picker.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    errMessage: PropTypes.string.isRequired,
+    err: PropTypes.object.isRequired,
+    format: PropTypes.string,
+    onChange: PropTypes.func
+};
+
+export default Picker;
