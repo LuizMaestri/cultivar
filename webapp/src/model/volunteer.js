3f0bedbf128182e5ecd9377e94224ac7864e48cd
LuizMaestri <luizricardomaestri@gmail.com>
2018-08-22 00:42:11 -0300

    volunteer register

--- webapp/src/model/volunteer.js
+++ webapp/src/model/volunteer.js
@@ -6,10 +6,12 @@ import Address from './address';
 
 export default class extends User {
     constructor(id = '', name = '', status = Status.WAIT_TV, email = '', password = '', phone = '',
-        birth = new Date().toISOString(), createAt = new Date().toISOString(), address = new Address({}), company = new Place(), pathTV = '', pathTR = '') {
+        birth = new Date().toISOString(), createAt = new Date().toISOString(), address = new Address({}), company = new Place(),
+        job = '', pathTV = '', pathTR = '') {
         super(id, name,Roles.VOLUNTEER, status, email, password, phone, birth, createAt);
         this.address = address;
         this.company = company;
+        this.job = job;
         this.pathTV = pathTV;
         this.pathTR = pathTR;
     }
--- webapp/src/pages/public/register/Form.jsx
+++ webapp/src/pages/public/register/Form.jsx
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import { Volunteer } from '../../../model';
 import { Button, Form, FormGroup, Label, Input, FormFeedback, Row, Col } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
-import { Mask, DatePicker } from '../../../components';
-import { getRequest } from '../../../utils/http';
-import axios from 'axios';
-
-const request = url => getRequest(url, res => res.data, () => [])
+import { Mask, DatePicker, Required } from '../../../components';
+import { getRequest, postRequest } from '../../../utils/http';
+import neighborhoods from '../../../utils/neighborhoods';
 
 export default class extends Component{
     constructor(){
         super();
         this.state = {
-            user: new Volunteer(),
+            volunteer: new Volunteer(),
             err: {
                 cpfErr: {},
                 passwordErr: {},
@@ -20,83 +18,182 @@ export default class extends Component{
                 nameErr: {},
                 companyErr: {},
                 jobErr: {},
-                emailErr: {}
+                emailErr: {},
+                cityErr: {},
+                neighborhoodErr: {},
+                streetErr: {}
             },
             companies: [],
-            cities: [],
             confirmPassword: '',
-            back: false
+            back: false,
+            cities: [
+                'Florianópolis',
+                'Palhoça',
+                'São José'
+            ],
+            neighborhoods
         };
     }
 
     componentWillMount(){
-        axios.all([request('/place/company'), request('/address/city')])
-            .then(res => this.setState({ companies: res[0], cities: res[1]}));
+        getRequest('/place/company', res => this.setState({ companies: res.data}));
     }
 
     handlerCpf(event){
-        const { user, err } = this.state;
-        user.id = event.target.value.replace(/[-.]/g, '');
-        err.cpfErr = { invalid: !user.id};
-        this.setState({ user, err });
+        const { volunteer, err } = this.state;
+        volunteer.id = event.target.value.replace(/[-.]/g, '');
+        err.cpfErr = { invalid: !volunteer.id};
+        this.setState({ volunteer, err });
+    }
+
+    handlerPhone(event) {
+        const { volunteer, err } = this.state;
+        volunteer.phone = event.target.value.replace(/[-()]/g, '')
+        err.phoneErr = { invalid: !volunteer.phone };
+        this.setState({ volunteer, err });
     }
 
     handlerName(event){
-        const { user, err } = this.state;
-        user.name = event.target.value;
-        err.nameErr = { invalid: !user.name};
-        this.setState({ user, err });
+        const { volunteer, err } = this.state;
+        volunteer.name = event.target.value;
+        err.nameErr = { invalid: !volunteer.name};
+        this.setState({ volunteer, err });
     }
 
     handlerPassword(event){
-        const { user, err } = this.state;
-        user.password = event.target.value;
-        err.passwordErr = { invalid: user.password !== this.state.confirmPassword }
-        this.setState({ user, err });
+        const { volunteer, err } = this.state;
+        volunteer.password = event.target.value;
+        err.passwordErr = { invalid: volunteer.password !== this.state.confirmPassword }
+        this.setState({ volunteer, err });
     }
 
     handlerConfirmPassword(event) {
-        const { user, err } = this.state;
+        const { volunteer, err } = this.state;
         let confirmPassword = this.state.confirmPassword;
         confirmPassword = event.target.value;
-        err.passwordErr = { invalid: user.password !== confirmPassword }
+        err.passwordErr = { invalid: volunteer.password !== confirmPassword }
         this.setState({ confirmPassword, err });
     }
 
     handlerCompany(event){
-        const { user, err } = this.state;
+        const { volunteer, err } = this.state;
         let companyId = event.target.value;
-        if(companyId !== null){
-            for (const company of this.state.companies) {
-                if (company.id === companyId){
-                    user.company = company;
-                    break;
-                }
-            }
-        } else {
-            user.company = companyId;
-        }
+        volunteer.company.id = companyId;
         err.companyErr = { invalid: !companyId }
-        this.setState({ user, err })
+        this.setState({ volunteer, err })
     }
 
     handlerJob(event){
-        const { user, err } = this.state;
-        user.job = event.target.value;
-        err.jobdErr = { invalid: !user.job }
-        this.setState({ user, err });
+        const { volunteer, err } = this.state;
+        volunteer.job = event.target.value;
+        err.jobdErr = { invalid: !volunteer.job }
+        this.setState({ volunteer, err });
     }
 
     handlerEmail(event){
-        const { user, err } = this.state;
+        const { volunteer, err } = this.state;
         let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        user.email = event.target.value;
-        err.emailErr = { invalid: !re.test(user.email) }
-        this.setState({ user, err });
+        volunteer.email = event.target.value;
+        err.emailErr = { invalid: !re.test(volunteer.email) }
+        this.setState({ volunteer, err });
+    }
+
+    handlerBirth(event) {
+        const { volunteer, err } = this.state;
+        const { value } = event.target;
+        volunteer.birth = value ? new Date(value).toISOString() : value;
+        err.birthErr = { invalid: !volunteer.birth }
+        this.setState({ volunteer, err });
+    }
+
+    // Address
+    handlerCity(event) {
+        const { volunteer, err } = this.state;
+        const { address } = volunteer;
+        address.city = event.target.value;
+        err.cityErr = { invalid: !address.city };
+        volunteer.address = address;
+        this.setState({ volunteer, err });
+    }
+
+    handlerNeighborhood(event) {
+        const { volunteer, err } = this.state;
+        const { address } = volunteer;
+        address.neighborhood = event.target.value;
+        err.neighborhoodErr = { invalid: !address.neighborhood };
+        volunteer.address = address;
+        this.setState({ volunteer, err });
+    }
+
+    handlerStreet(event) {
+        const { volunteer, err } = this.state;
+        const { address } = volunteer;
+        address.street = event.target.value;
+        err.streetErr = { invalid: !address.street };
+        volunteer.address = address;
+        this.setState({ volunteer, err });
+    }
+
+    handlerNumber(event) {
+        const { volunteer } = this.state;
+        volunteer.address.number = event.target.value;
+        this.setState({ volunteer });
+    }
+
+    handlerSubmit(){
+        const { volunteer, confirmPassword} = this.state;
+        const { address, company } = volunteer;
+        if(volunteer.id && volunteer.birth && volunteer.email && volunteer.name && 
+            volunteer.password && volunteer.job && company.id && address.city && 
+            address.neighborhood && address.street){
+            postRequest('/volunteer', volunteer, 
+                () => {
+                    alert("Cadastro realizado com sucesso.");
+                    this.setState({ back: true })
+                },
+                res => alert(res.data)
+            );
+        } else {
+            this.setState({
+                err: {
+                    cpfErr: {
+                        invalid: !volunteer.id
+                    },
+                    passwordErr: {
+                        invalid: (!volunteer.password || !confirmPassword) || volunteer.password !== confirmPassword
+                    },
+                    nascErr: {
+                        invalid: !volunteer.birth
+                    },
+                    nameErr: {
+                        invalid: !volunteer.name
+                    },
+                    companyErr: {
+                        invalid: !company.id
+                    },
+                    jobErr: {
+                        invalid: !volunteer.job
+                    },
+                    emailErr: {
+                        invalid: !volunteer.email
+                    },
+                    cityErr: {
+                        invalid: !address.city
+                    },
+                    neighborhoodErr: {
+                        invalid: !address.neighborhood
+                    },
+                    streetErr: {
+                        invalid: !address.street
+                    }
+                }
+            })
+        }
     }
 
     render(){
-        const { err, companies, back } = this.state;
+        const { err, companies, back, volunteer, cities } = this.state;
+        const { address } = volunteer;
         if (!back){
             return (
                 <Form>
@@ -105,26 +202,27 @@ export default class extends Component{
                         <Label for="name">Nome Completo</Label>
                         <Input id="name" onChange={this.handlerName.bind(this)} {...err.nameErr} />
                     </FormGroup>
-                    <DatePicker id="date" label="Nascimento" errMessage="Data inválida" err={err.nascErr}/>
-                    
+                    <DatePicker id="date" label="Nascimento" errMessage="Data inválida" onChange={this.handlerBirth.bind(this)} err={err.nascErr}/>
                     <Mask id="cpf" label="CPF" onChange={this.handlerCpf.bind(this)} mask="999.999.999-99" type="tel" placeholder="###.###.###-##"
                         err={err.cpfErr} errMessage="CPF inválido" />
-                    <FormGroup>
-                        <Label for="company">Empresa</Label>
-                        <Input type="select" name="company" id="exampleSelect" onSelect={this.handlerCompany.bind(this)} {...err.companyErr}>
-                            <option>Selecione</option>
-                            {!companies.length ? companies.map(company => (<option value={company.id}>{company.name}</option>)): null}
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="job">Função</Label>
-                        <Input id="job" onChange={this.handlerJob.bind(this)} disabled={!this.state.user.company.id} {...err.jobErr} />
-                        <FormFeedback tooltip></FormFeedback>
-                    </FormGroup>
+                    <Mask id="phone" label="Número p/ Contato" onChange={this.handlerPhone.bind(this)} mask="(99)9999-9999" placeholder="(##)####-####" type="tel" err={err.phoneErr}
+                        errMessage="Número p/ Contato é Obrigatório" />
                     <FormGroup>
                         <Label for="email">E-mail</Label>
                         <Input id="email" type="email" onChange={this.handlerEmail.bind(this)} {...err.emailErr} />
                         <FormFeedback tooltip>Email inválido</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="company">Empresa</Label>
+                        <Input type="select" name="company" id="exampleSelect" onChange={this.handlerCompany.bind(this)} {...err.companyErr}>
+                            <option>Selecione</option>
+                            {companies.map(company => (<option value={company.id}>{company.name}</option>))}
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="job">Função</Label>
+                        <Input id="job" onChange={this.handlerJob.bind(this)} disabled={!volunteer.company.id} {...err.jobErr} />
+                        <FormFeedback tooltip></FormFeedback>
                     </FormGroup>
                     <FormGroup>
                         <Label for="password">Senha</Label>
@@ -140,14 +238,48 @@ export default class extends Component{
                         </Row>
                     </FormGroup>
                     <FormGroup>
+                        <Label for="city">Cidade <Required /></Label>
+                        <Input type="select" name="city" id="city" onChange={this.handlerCity.bind(this)} {...err.cityErr}>
+                            <option value="">Selecione</option>
+                            {cities.length ? cities.map(city => (<option key={city} value={city}>{city}</option>)) : null}
+                        </Input>
+                        <FormFeedback tooltip>Cidade é Obrigatório</FormFeedback>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="neighborhood">Bairro <Required /></Label>
+                        <Input type="select" name="neighborhood" id="neighborhood" onChange={this.handlerNeighborhood.bind(this)} disabled={!address.city} {...err.neighborhoodErr}>
+                            <option value="">Selecione</option>
+                            {
+                                neighborhoods.hasOwnProperty(address.city) && neighborhoods[address.city].length ?
+                                    neighborhoods[address.city].map(neighborhood => (<option key={neighborhood} value={neighborhood}>{neighborhood}</option>)) : null
+                            }
+                        </Input>
+                        <FormFeedback tooltip>Bairro é Obrigatório</FormFeedback>
+                    </FormGroup>
+                    <Row>
+                        <Col md={10}>
+                            <FormGroup>
+                                <Label for="street">Lougradouro <Required /></Label>
+                                <Input id="street" onChange={this.handlerStreet.bind(this)} disabled={!address.neighborhood} {...err.streetErr} />
+                                <FormFeedback tooltip>Lougradouro é Obrigatório.</FormFeedback>
+                            </FormGroup>
+                        </Col>
+                        <Col md={2}>
+                            <FormGroup>
+                                <Label for="number">Número</Label>
+                                <Input id="number" onChange={this.handlerNumber.bind(this)} disabled={!address.neighborhood} />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <FormGroup>
                         <Row>
                             <Col>
-                                <Button id="cancel" type="button" color="danger" onClick={e => this.setState({back: true})}>
+                                <Button id="cancel" type="button" color="danger" onClick={() => this.setState({back: true})}>
                                     Voltar
                                 </Button>
                             </Col>
                             <Col>
-                                <Button id="submit" type="submit" color="primary" className="float-right">
+                                <Button id="submit" type="button" color="primary" className="float-right" onClick={this.handlerSubmit.bind(this)}>
                                     Cadastrar
                                 </Button>
                             </Col>
