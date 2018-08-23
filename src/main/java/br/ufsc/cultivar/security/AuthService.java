864ca4465b73903df67a5a30df1f5aff87553e6c
LuizMaestri <luizricardomaestri@gmail.com>
2018-08-23 01:23:34 -0300

    login no more hard code

--- src/main/java/br/ufsc/cultivar/security/AuthService.java
+++ src/main/java/br/ufsc/cultivar/security/AuthService.java
@@ -1,6 +1,7 @@
 package br.ufsc.cultivar.security;
 
 //import br.ufsc.cultivar.exception.ServiceException;
+import br.ufsc.cultivar.exception.ServiceException;
 import br.ufsc.cultivar.models.User;
 import br.ufsc.cultivar.service.UserService;
 import lombok.AccessLevel;
@@ -16,16 +17,15 @@ class AuthService {
     UserService userService;
 
     AuthResponseDTO authenticate(AuthRequestDTO dto) throws AuthException {
-//        try {
-//            User user = userService.get(dto.getCpf());
-            User user = User.builder().password("123456").build();
+        try {
+            User user = userService.get(dto.getCpf());
             if (user.getPassword().equals(dto.getPassword())) {
                 return AuthResponseDTO.builder()
                         .user(user)
                         .token("")
                         .build();
             }
-//        } catch (ServiceException ignored) {}
+        } catch (ServiceException ignored) {}
         throw new  AuthException("Usuário e/ou senha incorretos.");
     }
 }
