a268afa95e36f9f3a0d4efd8d7944ce8069b8970
LuizMaestri <luizricardomaestri@gmail.com>
2018-08-23 00:59:51 -0300

    fix save on upload file

--- src/main/java/br/ufsc/cultivar/service/FileService.java
+++ src/main/java/br/ufsc/cultivar/service/FileService.java
@@ -3,11 +3,15 @@ package br.ufsc.cultivar.service;
 import br.ufsc.cultivar.exception.ServiceException;
 import br.ufsc.cultivar.exception.Type;
 import lombok.extern.java.Log;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author luiz.maestri
@@ -18,31 +22,22 @@ import java.io.IOException;
 public class FileService {
 
     public String save(String dir, String name, MultipartFile multipart) throws ServiceException {
-        String originalFileName = multipart.getOriginalFilename();
-        String extesion = originalFileName.substring(originalFileName.lastIndexOf("."));
-        String fileName = String.format("files/cultivar/%s/%s.%s", dir, name, extesion);
+        String fileName = String.format("./files/cultivar/%s/%s.pdf", dir, name);
         return save(new File(fileName), multipart);
     }
 
     public String save(String dir, MultipartFile multipart) throws ServiceException {
-        String fileName = String.format("files/cultivar/%s/%s", dir, multipart.getOriginalFilename());
+        String fileName = String.format("./files/cultivar/%s/%s", dir, multipart.getOriginalFilename());
         return save(new File(fileName), multipart);
     }
 
     private String save(File file, MultipartFile multipart) throws ServiceException{
         try {
-//            if (file.getParentFile().mkdirs()){
-//                log.info("criado diretório" + file.getParent());
-//            }
-//            if(file.createNewFile()) {
-//                    multipart.transferTo(file);
-//                    return file.getName();
-//            }
-            multipart.transferTo(file);
+            if (file.getParentFile().mkdirs()){
+                log.info("create dir: " + file.getParent());
+            }
+            Files.copy(multipart.getInputStream(), Paths.get(file.toURI()));
             return file.getName();
-//            else {
-//                throw createException(multipart.getOriginalFilename(), null);
-//            }
         } catch (IOException e) {
             throw createException(multipart.getOriginalFilename(), e);
         }
