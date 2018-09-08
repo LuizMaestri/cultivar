98b4f46e8a7207a3c452f5ac2d5ca4e84e17c298
LuizMaestri <luizricardomaestri@gmail.com>
2018-09-08 14:39:04 -0300

    add method to translate Status enum to natural language

--- webapp/src/model/status.js
+++ webapp/src/model/status.js
@@ -11,7 +11,16 @@ const Status = {
         Status.WAIT_TRAINING,
         Status.REGISTER
     ],
-    has: status => Status.values().includes(status)
+    has: status => Status.values().includes(status),
+    translate: status => {
+        return {
+            APPROVED: 'Aprovado',
+            WAIT_STATEMENT: 'Aguardando envio do termo de resposabilidade',
+            WAIT_COMPANY: 'Aguardando recomendação da empresa',
+            WAIT_TRAINING: 'Aguardando treinamento',
+            REGISTER: 'Registrado',
+        }[status];
+    }
 };
 
 export default Status;
