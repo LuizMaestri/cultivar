fa70310604e669a8350224d99da520cd0d7d1701
LuizMaestri <luizricardomaestri@gmail.com>
2018-09-27 01:09:37 -0300

    fix attr name

--- webapp/src/components/form/checkbox/Checkbox.jsx
+++ webapp/src/components/form/checkbox/Checkbox.jsx
@@ -4,6 +4,6 @@ import './checkbox.css';
 export default ({ value, label, disabled, checked, onChange}) => (
     <label className="control control--checkbox">{label}
         <input type="checkbox" value={value} disabled={disabled} checked={checked} onChange={onChange}/>
-        <div class="control__indicator"></div>
+        <div className="control__indicator"></div>
     </label>
 )
