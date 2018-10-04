4ebb78ad9ed787514afb3d468e626a200b10be7c
LuizMaestri <luizricardomaestri@gmail.com>
2018-10-04 00:52:28 -0300

    new admin page style with collapse

--- webapp/src/pages/protected/admin/Page.jsx
+++ webapp/src/pages/protected/admin/Page.jsx
@@ -1,51 +1,88 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Button, Collapse } from 'reactstrap';
 import ListAttachment from './attachment';
 import ListQuestion from './question';
 import ListEventType from './eventType';
 import ListProject from './project';
 import { Roles } from '../../../model';
 
-const Hr = () => (
-    <Fragment>
-        <br />
-        <hr />
-        <br />
-    </Fragment>
-);
+const styleButton = {
+    width: '100%'
+};
 
-export default ({role}) => {
-    if(role === Roles.ADMIN){
+export default class extends Component{
+    constructor(){
+        super();
+        this.state = {
+            show: {
+                project: false,
+                eventType: false,
+                attachment: false,
+                question: false
+            }
+        };
+    }
+
+    changeShow(collapse) {
+        const { show } = this.state;
+        for (const key in show) {
+            if (show.hasOwnProperty(key)) {
+                show[key] = key === collapse ? !show[key] : false;
+            }
+        }
+        this.setState({ show })
+    }
+    
+    render(){
+        const { role } = this.props;
+        const { show } = this.state;
+        if (role === Roles.ADMIN) {
+            return (
+                <Row>
+                    <Col md="1" />
+                    <Col>
+                        <Row>
+                            <Col>
+                                <Button type="button" color="info" style={styleButton} onClick={() => this.changeShow('project')}>Projetos</Button>
+                            </Col>
+                            <Col md="1" />
+                            <Col>
+                                <Button type="button" color="info" style={styleButton} onClick={() => this.changeShow('eventType')}>Tipos de Evento</Button>
+                            </Col>
+                            <Col md="1" />
+                            <Col>
+                                <Button type="button" color="info" style={styleButton} onClick={()=>this.changeShow('attachment')}>Anexos</Button>
+                            </Col>
+                            <Col md="1" />
+                            <Col>
+                                <Button type="button" color="info" style={styleButton} onClick={()=>this.changeShow('question')}>Questões</Button>
+                            </Col>
+                        </Row>
+                        <br/>
+                        <Row>
+                            <Col>
+                                <Collapse isOpen={show.project}>
+                                    <ListProject />
+                                </Collapse>
+                                <Collapse isOpen={show.eventType}>
+                                    <ListEventType />
+                                </Collapse>
+                                <Collapse isOpen={show.attachment}>
+                                    <ListAttachment />
+                                </Collapse>
+                                <Collapse isOpen={show.question}>
+                                    <ListQuestion />
+                                </Collapse>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col md="1" />
+                </Row>
+            );
+        }
         return (
-            <Row>
-                <Col md="1" />
-                <Col>
-                    <Row style={{height: '400px', maxHeight: '400px'}}>
-                        <Col>
-                            <ListProject />
-                        </Col>
-                        <Col md="1" />
-                        <Col>
-                            <ListEventType />
-                        </Col>
-                    </Row>
-                    <Hr/>
-                    <Row style={{ height: '400px', maxHeight: '400px' }}>
-                        <Col>
-                            <ListAttachment />
-                        </Col>
-                    </Row>
-                    <Hr/>
-                    <Row style={{maxHeight: '400px' }}>
-                        <Col>
-                            <ListQuestion />
-                        </Col>
-                    </Row>
-                </Col>
-                <Col md="1" />
-            </Row>
+            <Redirect to="/dashboard" />
         )
     }
-    return <Redirect to="/dashboard"/>
-};
+}
